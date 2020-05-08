bfd2d7e0f1dd2b4cf909bf5f87053b9797fb1728
Tim Thompson <me@timthompson.com>
2020-05-08 15:58:06 -0700

    Don't use GOPATH

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 rem copy vs2019\build\morph\x64\Debug\morph.exe ..\viztopia\world1\bin\morphdebug.exe
 copy vs2019\build\morph\x64\Release\morph.exe ..\viztopia\world1\bin\morph.exe
+copy vs2019\build\morph\x64\Release\LibSensel*.dll ..\viztopia\world1\bin\
