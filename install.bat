4e54253b4bf869c8d0fed205730d3121c645670b
Tim Thompson <me@timthompson.com>
2020-05-09 16:23:12 -0700

    adapt to new layout in viztopia

--- install.bat
+++ install.bat
@@ -1,3 +1,2 @@
-rem copy vs2019\build\morph\x64\Debug\morph.exe ..\viztopia\world1\bin\morphdebug.exe
-copy vs2019\build\morph\x64\Release\morph.exe ..\viztopia\world1\bin\morph.exe
-copy vs2019\build\morph\x64\Release\LibSensel*.dll ..\viztopia\world1\bin\
+copy vs2019\build\morph\x64\Release\morph.exe ..\viztopia\root\bin\morph.exe
+copy vs2019\build\morph\x64\Release\LibSensel*.dll ..\viztopia\root\bin\
